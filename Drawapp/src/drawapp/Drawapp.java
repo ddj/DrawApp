cdbae9c56d7a4224a980f090e416cc918554c0f9
David Johnson <dd.j@btinternet.com>
2012-11-13 12:54:22 +0000

    Replaced Swing GUI with JavaFX Stage and Scene features

--- Drawapp/src/drawapp/Drawapp.java
+++ Drawapp/src/drawapp/Drawapp.java
@@ -5,26 +5,25 @@ import java.awt.Color;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Drawapp
+public class Drawapp extends Application
 {
-  public static void main(String[] args)
-  {
-    final MainWindow main = new MainWindow();
-
-    SwingUtilities.invokeLater(
-    new Runnable()
-    {
-      public void run()
-      {
-        ImagePanel imagePanel = main.getImagePanel();
-        Reader reader = new InputStreamReader(System.in);
-        Parser parser = new Parser(reader,imagePanel,main);
-        parser.parse();
-        imagePanel.repaint();
-      }
+  public static void main(String[] args) {
+        Application.launch(args);
     }
-  );
 
-  }
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Draw App");
+        Group root = new Group();
+        Scene scene = new Scene(root, 640, 480);
+    
+    primaryStage.setScene(scene);
+
+    primaryStage.show();
+}
 }
