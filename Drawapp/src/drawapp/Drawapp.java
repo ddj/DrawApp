193f0bf7c65b51b62e6dafc9f739c153fd3e4e44
David Johnson <dd.j@btinternet.com>
2012-11-19 23:20:03 +0000

    Question 1: Fully replaced interface with JavaFX equivalent functionality and features.

--- Drawapp/src/drawapp/Drawapp.java
+++ Drawapp/src/drawapp/Drawapp.java
@@ -1,29 +1,22 @@
 package drawapp;
 
-import javax.swing.SwingUtilities;
-import java.awt.Color;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Drawapp extends Application
-{
-  public static void main(String[] args) {
-        Application.launch(args);
+/**
+ *
+ * @author David
+ */
+public class Drawapp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Draw App");
-        Group root = new Group();
-        Scene scene = new Scene(root, 640, 480);
-    
-    primaryStage.setScene(scene);
-
-    primaryStage.show();
-}
+        MainWindow mainWindow = new MainWindow(primaryStage);
+        ImagePanel imagePanel = mainWindow.getImagePanel();
+        primaryStage.show();
+    }
 }
