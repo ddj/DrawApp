0fd004b9de791193904ce120da0a6f912deb97bb
David Johnson <dd.j@btinternet.com>
2012-11-19 23:29:50 +0000

    Revert "Revert "Question 1: Fully replaced interface with JavaFX equivalent functionality and features.""
    This reverts commit 16e6b5b53d5256c7fe26151d13d403abb555ee73.

--- Drawapp/src/drawapp/ImagePanel.java
+++ Drawapp/src/drawapp/ImagePanel.java
@@ -1,81 +1,85 @@
 package drawapp;
 
-import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
-public class ImagePanel extends JPanel
-{
-  private BufferedImage image;
-  private Graphics graphics;
+/**
+ *
+ * @author David
+ */
+public class ImagePanel extends HBox {
 
-  public ImagePanel(int width, int height)
-  {
-    setImageSize(width, height);
-  }
+    private Paint stringColour = Color.BLACK;
+    private Group picture = new Group();
 
-  private void setImageSize(int width, int height)
-  {
-    image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    graphics = image.getGraphics();
-    clear(Color.white);
-    setPreferredSize(new Dimension(width, height));
-  }
+    public ImagePanel(int width, int height) {
+        setImageSize(width, height);
+    }
 
-  @Override
-  protected void paintComponent(Graphics g)
-  {
-    g.setColor(Color.gray);
-    g.fillRect(0, 0, image.getWidth(), image.getHeight());
-    g.drawImage(image,0,0,this);
-  }
+    private void setImageSize(int width, int height) {
+        this.setPrefHeight(height);
+        this.setPrefWidth(width);
+    }
 
-  public void setBackgroundColour(Color colour)
-  {
-    graphics.setColor(colour);
-    graphics.fillRect(0, 0, image.getWidth(), image.getHeight());
-    graphics.setColor(Color.black);
-  }
+    public void add(Node g) {
+        this.getChildren().add(g);
+    }
 
-  public void clear(Color colour)
-  {
-    setBackgroundColour(colour);
-  }
+    public void setColour(Color colour) {
+        stringColour = colour;
+    }
 
-  public void setColour(Color colour)
-  {
-    graphics.setColor(colour);
-  }
+    public void drawLine(int x1, int y1, int x2, int y2) {
+        Line line = new Line();
+        line.setStartX(x1);
+        line.setStartY(y1);
+        line.setEndX(x2);
+        line.setEndY(y2);
+        line.setStroke(stringColour);
+        picture.getChildren().add(line);
+        stringColour = Color.BLACK;
+    }
 
-  public void drawLine(int x1, int y1, int x2, int y2)
-  {
-    graphics.drawLine(x1, y1, x2, y2);
-  }
+    public void drawRect(int x1, int y1, int x2, int y2) {
+        Rectangle rect = new Rectangle(x1, y1, x2, y2);
+        rect.setStroke(Paint.valueOf("000000"));
+        rect.setFill(Paint.valueOf("00000000"));
+        picture.getChildren().add(rect);
+    }
 
-  public void drawRect(int x1, int y1, int x2, int y2)
-  {
-    graphics.drawRect(x1, y1, x2, y2);
-  }
+    public void fillRect(int x1, int y1, double x2, double y2) {
+        Rectangle rectFill = new Rectangle(x1, y1, x2, y2);
+        rectFill.setFill(stringColour);
+        picture.getChildren().add(rectFill);
+        stringColour = Color.BLACK;
+    }
 
-  public void fillRect(int x1, int y1, int x2, int y2)
-  {
-    graphics.fillRect(x1, y1, x2, y2);
-  }
+    public void drawString(int x, int y, String s) {
+        Text t = new Text(x, y, s);
+        picture.getChildren().add(t);
+    }
 
-  public void drawString(int x, int y, String s)
-  {
-    graphics.drawString(s,x,y);
-  }
+    public void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
+        Arc arc = new Arc(x, y, width / 2, height / 2, startAngle, arcAngle);
+        arc.setStroke(stringColour);
+        arc.setFill(Paint.valueOf("00000000"));
+        picture.getChildren().add(arc);
+        stringColour = Color.BLACK;
+    }
 
-  public void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle)
-  {
-    graphics.drawArc(x,y,width,height,startAngle,arcAngle);
-  }
-
-  public void drawOval(int x, int y, int width, int height)
-  {
-    graphics.drawOval(x,y,width,height);
-  }
+    public void drawOval(int x, int y, int width, int height) {
+        Ellipse oval = new Ellipse(x, y, width, height);
+        oval.setStroke(stringColour);
+        oval.setFill(Paint.valueOf("00000000"));
+        picture.getChildren().add(oval);
+        stringColour = Color.BLACK;
+    }
 }
